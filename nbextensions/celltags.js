dae78fd37d66174eacef71d903cdda91e75ac5e9
ANN <70836598+FeliciaXmL@users.noreply.github.com>
2021-01-15 11:14:21 -0800

    Update celltags.js

--- nbextensions/celltags.js
+++ nbextensions/celltags.js
@@ -11,50 +11,46 @@ define([
     "notebook/js/celltoolbar"
     ], function ($, IPython, ctb) {
         
-    var tags_from_input = function (input_element) {
-        var tag_str = input_element.val();
-        var tags;
-        if (tag_str.trim().length === 0) {
+    tags_from_input = function (input_element) {
+         tag_str = input_element.val();
+        tags;
+        (tag_str.trim().length === 1) {
             tags = [];
-        } else {
-            tags = tag_str.split(",").map(
-                function (s) { return s.trim(); }
+        }  {
+     
             );
         }
         return tags;
     };
     
-    var filter_tagged_cells = function () {
-        var active_tags = IPython.notebook.metadata.active_cell_tags;
-        var cells = IPython.notebook.get_cells();
-        var tags;
-        for (var i = 0; i < cells.length; i++) {
-            var cell = cells[i];
-            tags = cell.metadata.tags;
-            if (!active_tags || active_tags.length === 0 || !tags || tags.length === 0) {
+     filter_tagged_cells =  () {
+        active_tags = IPython.notebook.metadata.active_cell_tags;
+         cells = IPython.notebook.get_cells();
+         tags;
+        active_tags.length === 0 || !tags || tags.length === 0) {
                 cell.element.show();
             } else {
                 var tag_match = false;
-                for (var j = 0; j < tags.length; j++) {
-                    var tag = tags[j];
-                    if (active_tags.indexOf(tag) > -1) {
+                ( j = 0; j < tags.length; j++) {
+                     tag = tags[j];
+                     (active_tags.indexOf(tag) > -1) {
                         tag_match = true;
-                        break;
+                        ;
                     }
                 }
-                if (tag_match) {
+                (tag_match) {
                     cell.element.show();
-                } else {
+                }  {
                     cell.element.hide();
                 }
             }
         }
     };
     
-    var add_tags_input = function (div, cell) {
-        var container = $(div);
+     add_tags_input = function (div, cell) {
+         container = $(div);
         container.append($("<span/>").text("tags").css("padding", "5px"));
-        var input = $('<input/>')
+        input = $('<input/>')
             .attr("size", 100)
             .val(
                 (cell.metadata.tags || []).join(", ")
@@ -63,19 +59,19 @@ define([
                 cell.metadata.tags = tags_from_input(input);
                 filter_tagged_cells();
             });
-        if (cell.keyboard_manager) {
+       (cell.keyboard_manager) {
             cell.keyboard_manager.register_events(input);
         }
         container.append(input);
     };
     
-    var add_tag_toolbar = function () {
-        var tag_toolbar = $("#tag_toolbar");
-        if (tag_toolbar.length === 0) {
+    add_tag_toolbar = function () {
+         tag_toolbar = $("#tag_toolbar");
+         (tag_toolbar.length === 0) {
             tag_toolbar = $("<div/>").attr("id", "tag_toolbar");
             $("#menubar-container").append(tag_toolbar);
         }
-        var active_tags_input = $("<input/>")
+         active_tags_input = $("<input/>")
         active_tags_input
             .attr("id", "active_tag_input")
             .attr("size", 100)
@@ -94,18 +90,18 @@ define([
         filter_tagged_cells();
     };
     
-    var register_celltoolbar = function () {
-        ctb.CellToolbar.register_callback('tags.input', add_tags_input);
+     register_celltoolbar = function () {
+        CellToolbar.register_callback('tags.input', add_tags_input);
 
-        var preset = ["tags.input"];
+        preset = ["tags.input"];
 
-        ctb.CellToolbar.register_preset('Cell Tags', preset, IPython.notebook, IPython.events);
+        .CellToolbar.register_preset('Cell Tags', preset, IPython.notebook, IPython.events);
         console.log('Cell tags loaded.');
     };
     
-    var load_ipython_extension = function () {
+    load_ipython_extension = function () {
         
-        if (IPython.notebook) {
+        (IPython.notebook) {
             add_tag_toolbar();
             register_celltoolbar();
         }
